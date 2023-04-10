8c2509b0e501bbcad1597dc940622fe316192d72
Samuel Mothwa <samuel@flatrocksolutions.co.za>
2023-04-10 20:24:32 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
 }
 
 module.exports = nextConfig
